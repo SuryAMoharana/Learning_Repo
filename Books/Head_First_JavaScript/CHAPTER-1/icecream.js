a312d08b265a779c520de1e8b3d633d0a9060112
Surya Moharana <suryachintu1994@gmail.com>
2024-02-04 23:56:22 +0530

    Chapter-1
    Ongoing

---
+++ Books/Head_First_JavaScript/CHAPTER-1/icecream.js
@@ -0,0 +1,9 @@
+
+function kitty(){
+    var scoop=5;
+    while(scoop>0){
+        console.log("Another scoop!<br>");
+        scoop=scoop-1;
+    }
+    document.write("Life without icecream is not the same");
+}
