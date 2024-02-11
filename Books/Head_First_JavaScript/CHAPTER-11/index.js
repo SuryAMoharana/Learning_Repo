3e0c3e24deda15a9b3359956a60b9a51c43faa97
Surya Moharana <suryachintu1994@gmail.com>
2024-02-11 22:00:39 +0530

    addon

--- Books/Head_First_JavaScript/CHAPTER-11/index.js
+++ Books/Head_First_JavaScript/CHAPTER-11/index.js
@@ -1,23 +1,27 @@
-var migrating = true;
-var fly = function(num) {
-    var sound = "Flying";
-    function wingFlapper() {
-        console.log(sound);
-    }
-    for (var i = 0; i < num; i++) {
-        wingFlapper();
-    }
-};
-function quack(num) {
-    var sound = "Quack";
-    var quacker = function() {
-    console.log(sound);
-    };
-    for (var i = 0; i < num; i++) {
-        quacker();
-    }
-}
-if (migrating) {
-    quack(4);
-    fly(4);
-}
+// var migrating = true;
+// var fly = function(num) {
+//     var sound = "Flying";
+//     function wingFlapper() {
+//         console.log(sound);
+//     }
+//     for (var i = 0; i < num; i++) {
+//         wingFlapper();
+//     }
+// };
+// function quack(num) {
+//     var sound = "Quack";
+//     var quacker = function() {
+//     console.log(sound);
+//     };
+//     for (var i = 0; i < num; i++) {
+//         quacker();
+//     }
+// }
+// if (migrating) {
+//     quack(4);
+//     fly(4);
+// }
+
+(function(hello){
+    console.log(hello);
+})("Hello")
